04a3ef41a246475dfe4fe91e2b9b5207de458298
Teo Nogueira <teonogueira17@gmail.com>
2022-08-29 17:15:41 -0300

    aula_03_formulario_modulo_7

---
+++ MODULO 7/form.js
@@ -0,0 +1,18 @@
+let nomeBeneficiario = document.getElementById('nome-beneficiario')
+
+let inputs = document.getElementByTagName('input')
+
+
+// document.getElementByTagName('input')[0].getElementByTagButton('button')
+
+// document.getElementByClassName('container');
+
+// document.querySelector('button')
+// document.querySelector('input')
+// document.querySelector('input[type=number]')
+// document.querySelectorAll('input#numero-conta')
+// document.querySelector('h1');
+
+
+// document.body
+// document.head
