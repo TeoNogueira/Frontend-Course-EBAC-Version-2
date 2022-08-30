a3b523394fb8ae1a0118ce377c592d15d10fcfe8
Teo Nogueira <teonogueira17@gmail.com>
2022-08-30 15:22:12 -0300

    form_aula03

--- MODULO 7/form.js
+++ MODULO 7/form.js
@@ -1,6 +1,23 @@
 let nomeBeneficiario = document.getElementById('nome-beneficiario')
 
-let inputs = document.getElementByTagName('input')
+
+nomeBeneficiario.value
+
+nomeBeneficiario.value.split('').length > 3
+
+document.getElementById('btn-depositar').disabled = false
+document.getElementById('btn-depositar').disabled = nomeBeneficiario.value.split(' ').length < 3
+
+let button = document.getElementById('btn-depositar')
+
+console.log(button)
+
+
+
+
+
+//  ----x--x-x-x-x-x--x------------------  ;//
+// let inputs = document.getElementByTagName('input')
 
 
 // document.getElementByTagName('input')[0].getElementByTagButton('button')
@@ -12,7 +29,16 @@ let inputs = document.getElementByTagName('input')
 // document.querySelector('input[type=number]')
 // document.querySelectorAll('input#numero-conta')
 // document.querySelector('h1');
-
-
+// nomeBeneficiario.value = "Teo Nogueira"
+// nomeBeneficiario.type
+// nomeBeneficiario.id
+// nomeBeneficiario.placeholder
 // document.body
 // document.head
+
+
+// let valorDeposito = document.getElementById('valor-deposito')
+
+
+// console.log(valorDeposito)
+
