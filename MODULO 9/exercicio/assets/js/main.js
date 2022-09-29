58a913fe43d7de1a2aae7a244b0d53d7d6e0eb17
Teo Nogueira <teonogueira17@gmail.com>
2022-09-29 17:36:24 -0300

    finished_classroom_1_carousel_mod_10_JqueryPlugins_slick

--- MODULO 9/exercicio/assets/js/main.js
+++ MODULO 9/exercicio/assets/js/main.js
@@ -6,7 +6,7 @@ $('#input').change(function() {
 var input = $(this).val();
  
 
-$('ul').append('<li>' + input + ' <i class="fas fa-check"></i> <i class="fas fa-trash"></i> </li>');
+$('ul').prepend('<li>' + input + ' <i class="fas fa-check"></i> <i class="fas fa-trash"></i> </li>');
 
 
 $(this).val('')
